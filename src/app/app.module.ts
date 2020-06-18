da063c9a113b62d386b2f72e138d1524d193a717
garypretell <35612447+garypretell@users.noreply.github.com>
2020-06-17 21:40:34 -0500

    agregando firestore...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDYOaBhbUkBc20baL-oGkppcZxy3Wqxb04',
+    authDomain: 'magnum-50f46.firebaseapp.com',
+    databaseURL: 'https://magnum-50f46.firebaseio.com',
+    projectId: 'magnum-50f46',
+    storageBucket: 'magnum-50f46.appspot.com',
+    messagingSenderId: '799541362116',
+    appId: '1:799541362116:web:6dba15ca860db61fd8f891',
+    measurementId: 'G-88N002VEJ8'
+  }
 };
